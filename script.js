2e99920483b9637755ef616e8a54b02d91b0334f
Josué Estrada <dev.josuestrd@gmail.com>
2023-03-08 01:25:18 -0500

    Agregado funcion arrow y dom.text.content

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btn = document.querySelector('[data-form-btn]');
 //console.log(btn)
-
-// Evento Listener
-btn.addEventListener('click', function () {
-    console.log('crear tarea');
+const createTask = (e) => {
+    e.preventDefault();
     const input = document.querySelector('[data-form-input]');
     console.log(input.value);
-});
+    document.querySelector('.task').textContent = input.value;
+}
+// Evento Listener
+btn.addEventListener('click', createTask);
