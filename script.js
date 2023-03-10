9dfb9f6f4f3607d351120fee5bf95033ada531ce
Josué Estrada <dev.josuestrd@gmail.com>
2023-03-09 21:15:10 -0500

    Agregado comentarios y explicaciones, div y span

--- script.js
+++ script.js
@@ -4,22 +4,39 @@ const createTask = (e) => {
     e.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
+    input.value = '';
     // Select parent element
     const list = document.querySelector('[data-list]');
+
     // Create child element
     const task = document.createElement('li');
+    const taskContent = document.createElement('div');
+    const titleTask = document.createElement('span');
+
     // Add classes to child element
     task.classList.add('card');
-    input.value = '';
-    const content = `<div>
-                        <i class="far fa-check-square icon"></i>
-                        <span class="task">${value}</span>
-                    </div>
-                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    titleTask.classList.add('task');
+
+    // Modify values
+    titleTask.innerText = value;
+    console.log(checkComplete());
+
+
+    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //task.innerHTML = content;
     // Add child element to parent element
     list.appendChild(task);
-    console.log(content);
+    task.appendChild(taskContent);
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    //console.log(content);
 }
 // Evento Listener
 btn.addEventListener('click', createTask);
+
+// Crear arrow function para el icono del check
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    return i;
+}
