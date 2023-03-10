4091f28c9faf03fdf0ae80585b4b72e50ab0c05a
Josué Estrada <dev.josuestrd@gmail.com>
2023-03-09 20:50:20 -0500

    Agregado comentarios

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ const createTask = (e) => {
     e.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
+    // Select parent element
     const list = document.querySelector('[data-list]');
+    // Create child element
     const task = document.createElement('li');
+    // Add classes to child element
     task.classList.add('card');
     input.value = '';
     const content = `<div>
-              <i class="far fa-check-square icon"></i>
-              <span class="task">${value}</span>
-            </div>
-            <i class="fas fa-trash-alt trashIcon icon"></i>`;
+                        <i class="far fa-check-square icon"></i>
+                        <span class="task">${value}</span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
     task.innerHTML = content;
+    // Add child element to parent element
     list.appendChild(task);
     console.log(content);
 }
