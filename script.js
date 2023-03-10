91a2b1466dc2f1b8a5ae4904ff081bd573e8cf4c
Josué Estrada <dev.josuestrd@gmail.com>
2023-03-10 02:46:25 -0500

    Agregado IIFE

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
-const btn = document.querySelector('[data-form-btn]');
+// Immediately Invoked Function Expression
+( () => {
+    const btn = document.querySelector('[data-form-btn]');
 //console.log(btn)
-const createTask = (e) => {
-    e.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const value = input.value;
-    input.value = '';
-    // Select parent element
-    const list = document.querySelector('[data-list]');
+    const createTask = (e) => {
+        e.preventDefault();
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;
+        input.value = '';
+        // Select parent element
+        const list = document.querySelector('[data-list]');
 
-    // Create child element
-    const task = document.createElement('li');
-    const taskContent = document.createElement('div');
-    const titleTask = document.createElement('span');
-    const trashIcon = document.createElement('i');
+        // Create child element
+        const task = document.createElement('li');
+        const taskContent = document.createElement('div');
+        const titleTask = document.createElement('span');
+        const trashIcon = document.createElement('i');
 
-    // Add classes to child element
-    task.classList.add('card');
-    titleTask.classList.add('task');
-    trashIcon.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+        // Add classes to child element
+        task.classList.add('card');
+        titleTask.classList.add('task');
+        trashIcon.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
 
-    // Modify values
-    titleTask.innerText = value;
-    console.log(checkComplete());
+        // Modify values
+        titleTask.innerText = value;
 
-    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
-    //task.innerHTML = content;
-    // Add child element to parent element
-    list.appendChild(task);
-    task.appendChild(taskContent);
-    task.appendChild(trashIcon);
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    //console.log(content);
-}
+        const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+        //task.innerHTML = content;
+        // Add child element to parent element
+        list.appendChild(task);
+        task.appendChild(taskContent);
+        task.appendChild(trashIcon);
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
+        //console.log(content);
+    };
 // Evento Listener
-btn.addEventListener('click', createTask);
+    btn.addEventListener('click', createTask);
 
 // Crear arrow function para el icono del check
-const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask);
-    return i;
-}
+    const checkComplete = () => {
+        const i = document.createElement('i');
+        i.classList.add('far', 'fa-check-square', 'icon');
+        i.addEventListener('click', completeTask);
+        return i;
+    };
 
 // Arrow function para cambiar el icono y color para tarea completada
-const completeTask = (event) => {
-    console.log(event.target);
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('far');
-    element.classList.toggle('completeIcon');
-}
+    const completeTask = (event) => {
+        // Encontrar el objetivo del evento utilizando la propiedad
+        const element = event.target;
+        element.classList.toggle('fas');
+        element.classList.toggle('far');
+        element.classList.toggle('completeIcon');
+    };
+})();
